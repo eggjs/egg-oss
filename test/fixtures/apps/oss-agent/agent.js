9c722b2925b45e44a24998c13cd2abf41a234436
popomore <sakura9515@gmail.com>
2017-02-03 14:50:42 +0800

    chore: upgrade deps and fix test

--- test/fixtures/apps/oss-agent/agent.js
+++ test/fixtures/apps/oss-agent/agent.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 module.exports = agent => {
   agent.messenger.on('oss start', () => {
-    co.call(agent, function*() {
+    co.call(agent, function* () {
       const name = 'chair-oss-test-upload-' + process.version + '-' + Date.now();
       yield this.oss.put(name, fs.createReadStream(__filename));
       this.messenger.broadcast('oss done');
