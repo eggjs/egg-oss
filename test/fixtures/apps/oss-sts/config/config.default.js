1bc0cd3901ee008b09722888bb5573ac9fca86ee
Haoliang Gao <sakura9515@gmail.com>
2017-10-11 22:59:05 -0500

    feat: support sts client (#5)
    when `config.sts === true`, it will create STSClient
    
    Closes https://github.com/eggjs/egg/issues/1476

---
+++ test/fixtures/apps/oss-sts/config/config.default.js
@@ -0,0 +1,6 @@
+'use strict';
+
+exports.keys = '123';
+exports.oss = {
+  client: Object.assign({ sts: true }, require('../../../../sts_config')),
+};
---
+++ test/sts_config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  accessKeyId: process.env.ALI_SDK_STS_ID,
+  accessKeySecret: process.env.ALI_SDK_STS_SECRET,
+  roleArn: process.env.ALI_SDK_STS_ROLE,
+  bucket: process.env.ALI_SDK_STS_BUCKET,
+  endpoint: process.env.TRAVIS ? 'https://sts.us-west-1.aliyuncs.com/' : null,
+};
