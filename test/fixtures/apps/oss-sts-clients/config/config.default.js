93a05eabba7d0a67f38170d626971341ef92fdc0
Haoliang Gao <sakura9515@gmail.com>
2018-11-18 22:27:24 +0800

    test: add another client for sts clients test (#9)

--- test/fixtures/apps/oss-sts-clients/config/config.default.js
+++ test/fixtures/apps/oss-sts-clients/config/config.default.js
@@ -4,5 +4,6 @@ exports.keys = '123';
 exports.oss = {
   clients: {
     client1: Object.assign({ sts: true }, require('../../../../sts_config')),
+    client2: Object.assign({}, require('../../../../config')),
   },
 };
