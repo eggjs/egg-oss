104ed915755de146427869756e43b4a10097f60a
fengmk2 <fengmk2@gmail.com>
2022-12-04 00:10:18 +0800

    f

--- index.test-d.ts
+++ index.test-d.ts
@@ -6,6 +6,8 @@ const client = {} as OSSClient;
 expectType<string>(client.signatureUrl('foo'));
 
 const app = {} as Application;
-expectType<OSSClient>(app.oss.get('oss-client-1'));
 expectType<string>(app.oss.signatureUrl('foo'));
 expectType<string>(await app.oss.asyncSignatureUrl('foo'));
+expectType<OSSClient>(app.oss.get('oss-client-1'));
+expectType<string>(app.oss.get('oss-client-1').signatureUrl('foo'));
+expectType<string>(await app.oss.get('oss-client-1').asyncSignatureUrl('foo'));
